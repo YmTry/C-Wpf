b7ee039b03c0944b643c7615ab1534c46a72a4e8
YmTry <1584661334@qq.com>
2019-06-03 23:58:05 +0800

    学习x:type标记扩展

---
+++ 学习WPF历程/X命名空间详解/MyButton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace X命名空间详解
+{
+    class MyButton :Button
+    {
+        public Type UserWindowType { get; set; }
+
+        protected override void OnClick()
+        {
+            base.OnClick();//激发click事件
+            Window win = Activator.CreateInstance(this.UserWindowType) as Window;
+            if (win!=null)
+            {
+                win.ShowDialog();
+            }
+        }
+    }
+}
