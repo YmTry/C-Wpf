d3cec024cad0fa38a515e5f9aff84c72343b4f6f
YmTry <1584661334@qq.com>
2019-06-11 19:00:35 +0800

    2019-6-11

---
+++ 学习WPF历程/X命名空间详解/Frames/GridTest.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace X命名空间详解.Frames
+{
+    /// <summary>
+    /// Interaction logic for GridTest.xaml
+    /// </summary>
+    public partial class GridTest : Window
+    {
+        public GridTest()
+        {
+            InitializeComponent();
+        }
+
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //this.GridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            //this.GridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            //this.GridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            //this.GridMain.ColumnDefinitions.Add(new ColumnDefinition());
+            //this.GridMain.RowDefinitions.Add(new RowDefinition());
+            //this.GridMain.RowDefinitions.Add(new RowDefinition());
+            //this.GridMain.RowDefinitions.Add(new RowDefinition());
+            //this.GridMain.ShowGridLines = true;
+            MessageBox.Show("123");
+        }
+        
+    }
+}
---
+++ 学习WPF历程/控件与布局/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace 控件与布局
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            List<Employee> empList = new List<Employee>()
+            {
+                new Employee(){Id = 1,Name = "Tom",Age = 30},
+                new Employee(){Id = 2,Name = "Yom",Age = 312},
+                new Employee(){Id = 3,Name = "Bom",Age = 4},
+                new Employee(){Id = 4,Name = "Zom",Age = 2},
+            };
+            this.ListBoxEmplyee.DisplayMemberPath = "Name";
+            this.ListBoxEmplyee.SelectedValue = "Id";
+            this.ListBoxEmplyee.ItemsSource = empList;
+            InitializeComponent();
+        }
+
+        //private void btn_Victor(object sender, RoutedEventArgs e)
+        //{
+        //    Button btn = sender as Button;
+        //    DependencyObject level1 = VisualTreeHelper.GetParent(btn);
+        //    DependencyObject level2 = VisualTreeHelper.GetParent(level1);
+        //    DependencyObject level3 = VisualTreeHelper.GetParent(level2);
+        //    MessageBox.Show(level3.GetType().ToString() + "\n" + level2.GetType().ToString() + "\n" + level1.GetType().ToString());
+
+        //}
+    }
+}
